12126b94e2e58e57c0d377bb2348c949e4dc15a9
Tal-D <tdan.m92@gmail.com>
2020-12-31 22:27:49 +0200

    Added an option to run the script with windows system

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/app-dir/local -w /app-dir/local contacts-fixer sh
+# Choose running machine name
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+# Mount repository path
+if [ $machine == "Linux" ]
+then
+	docker run --rm -it -v $(pwd):/app-dir/local -w /app-dir/local contacts-fixer sh
+else
+	windows_path=$(pwd | sed 's/^\///' | sed 's/\//\\/g' | sed 's/^./\0:/')
+	winpty docker run --rm -it -v $windows_path:/app-dir/local contacts-fixer sh
+fi
