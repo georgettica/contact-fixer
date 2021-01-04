f91457e02f721db283a7b88706c4f1840e3c1bf5
Tal-D <tdan.m92@gmail.com>
2021-01-04 18:30:13 +0200

    Refactor connections printing process: moved single connection print into a separated method.

--- lib/contact_fixer.rb
+++ lib/contact_fixer.rb
@@ -19,30 +19,34 @@ class ContactFixer
     end
   end
 
+  def print_connection(person)
+    names = person.names
+    if names.nil?
+      @output.puts "No names found for connection"
+    else
+      @output.puts names.map { |name| get_fixed_display_name(name.display_name) }.inspect
+    end
+    phone_numbers = person.phone_numbers
+    if phone_numbers.nil?
+      @output.puts "No numbers found for connection"
+    else
+      @output.puts "- " + phone_numbers.map { |phone_number| phone_number.value }.inspect
+    end
+    emails = person.email_addresses
+    if emails.nil?
+      @output.puts "No emails found for connection"
+    else
+      @output.puts "- " + emails.map { |email| email.value }.inspect
+    end
+    # newline is always good
+    @output.puts ""
+  end
+
   def print_connections(response)
     @output.puts "Connection names:"
     @output.puts "No connections found" if response.connections.empty?
     response.connections.each do |person|
-      names = person.names
-      if names.nil?
-        @output.puts "No names found for connection"
-      else
-        @output.puts names.map { |name| get_fixed_display_name(name.display_name) }.inspect
-      end
-      phone_numbers = person.phone_numbers
-      if phone_numbers.nil?
-        @output.puts "No numbers found for connection"
-      else
-        @output.puts "- " + phone_numbers.map { |phone_number| phone_number.value }.inspect
-      end
-      emails = person.email_addresses
-      if emails.nil?
-        @output.puts "No emails found for connection"
-      else
-        @output.puts "- " + emails.map { |email| email.value }.inspect
-      end
-      # newline is always good
-      @output.puts ""
+      print_connection(person)
     end
   end
 
