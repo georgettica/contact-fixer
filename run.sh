ba70bda0516357539096cab18509e509ef57b6c2
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-01-04 19:18:34 +0200

    fix(run): make run be macos supportive

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ case "$(uname -s)" in
 esac
 
 # Mount repository path
-if [ $machine == "Linux" ]
+if [[ $machine == "Linux"  || $machine == "Mac" ]]
 then
 	docker run --rm -it -v $(pwd):/app-dir/local -w /app-dir/local contacts-fixer sh
 else
