8d3f9db6d17b87de52fc1943ec29028cccd0f920
Tal-D <tdan.m92@gmail.com>
2021-01-08 08:35:21 +0200

    Added missing indentation and for loop closure.

--- lib/contact_fixer.rb
+++ lib/contact_fixer.rb
@@ -53,9 +53,10 @@ class ContactFixer
   def update_contacts_phone_numbers(contacts, raw_regex, substitute_pattern)
     regex = Regexp.new raw_regex
     contacts.each do |contact|
-    phone_numbers = contact.phone_numbers
-    unless phone_numbers.nil?
-      phone_numbers.each{|phone_number| phone_number.value = phone_number.value.gsub(regex, substitute_pattern)}
+      phone_numbers = contact.phone_numbers
+      unless phone_numbers.nil?
+        phone_numbers.each{|phone_number| phone_number.value = phone_number.value.gsub(regex, substitute_pattern)}
+      end
     end
   end
 
